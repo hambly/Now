c2462c9e39bdc5eb4f5a1c15bcdd820d7ee14203
Mark Hambly <hambly@gmail.com>
2014-03-04 17:20:47 -0800

    icon, load screen, time, and refresh added.

--- Now/NOWViewController.m
+++ Now/NOWViewController.m
@@ -10,6 +10,11 @@
 
 @interface NOWViewController ()
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UILabel *localTimeLabel;
+@property (weak, nonatomic) IBOutlet UIButton *refreshButton;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
+@property (strong, nonatomic) NSURL *imageURL;
+@property (strong, nonatomic) NSTimer *timer;
 
 @end
 
@@ -18,8 +23,37 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    [self.imageView setImage:[UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://imgs.xkcd.com/comics/now.png"]]]];
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:0.05 target:self selector:@selector(updateLocalTime) userInfo:nil repeats:YES];
+    self.imageURL = [NSURL URLWithString:@"http://imgs.xkcd.com/comics/now.png"];
+    [self loadImage];
 }
 
+- (IBAction)refreshButtonTouched:(id)sender {
+    [self loadImage];
+}
+
+- (void) loadImage {
+    [self.refreshButton setEnabled:NO];
+    [self.activityIndicator startAnimating];
+    
+    NSURLSession *session = [NSURLSession sharedSession];
+    [[session dataTaskWithURL:self.imageURL
+            completionHandler:^(NSData *data,
+                                NSURLResponse *response,
+                                NSError *error) {
+                
+                if (!error) {
+                    [self.imageView setImage: [UIImage imageWithData:data]];
+                }
+                [self.refreshButton setEnabled:YES];
+                [self.activityIndicator stopAnimating];
+            }] resume];
+}
+
+- (void) updateLocalTime {
+    NSDateFormatter *df = [[NSDateFormatter alloc] init];
+    [df setDateFormat:@"h:mm:ss a, z"];
+    self.localTimeLabel.text = [df stringFromDate:[NSDate date]];
+}
 
 @end
